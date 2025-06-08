9a1aa78eae4c585658fad1deacfd5312ce3b3583
ramakrishnan-git-dev <ramakrishnan_nagarajan@outlook.com>
2025-06-08 20:24:48 +0530

    Add files via upload

---
+++ inheritance/Program.cs
@@ -0,0 +1,28 @@
+﻿/*Title       : Inheritance
+  Author      : Ramakrishnan N
+  Created at  : 30/09/2022
+  Reviewed by : 
+  Reviewed at :*/
+using System;
+namespace ConsoleApplication {
+class SuperClass {
+	public void executeBaseClass()
+	{
+		Console.WriteLine("Base class executed");
+	}
+}
+class SubClass : SuperClass {
+	public void executeDerivedClass()
+	{
+		Console.WriteLine("Derived class executed");
+	}
+}
+class Inheritance {
+	static void Main(string[] args)
+	{
+		SubClass subClass = new SubClass();
+		subClass.executeDerivedClass();
+        subClass.executeBaseClass();
+	}
+}
+}
