0b023726313744eb05cffcf94db0b7db6655a139
ramakrishnan-git-dev <ramakrishnan_nagarajan@outlook.com>
2025-06-08 20:26:40 +0530

    Add files via upload

---
+++ encapsulation/Program.cs
@@ -0,0 +1,28 @@
+﻿/*Title       : Encapsulation
+  Author      : Ramakrishnan N
+  Created at  : 30/09/2022
+  Reviewed by : 
+  Reviewed at :*/
+using System;
+public class Encapsulation {
+	private int a;
+	public int num
+	{
+		get
+		{
+			return a;	
+		}
+		set
+		{
+			a = value;
+		}	
+	}	
+}
+class Execution{
+	static public void Main()
+	{
+		Encapsulation encapsulation = new Encapsulation();
+		encapsulation.num = 10;
+		Console.WriteLine(encapsulation.num);
+	}
+}
